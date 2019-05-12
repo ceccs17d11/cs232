6a72559f999413e6fe1db3d26e2f3f16528302e1
ceccs17d11 <43630274+ceccs17d11@users.noreply.github.com>
2019-05-12 12:43:41 +0530

    Add files via upload

---
+++ task 4/scpsshrsync.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+ssh cs17d11@192.168.0.30
+ls
+scp one.txt two.txt
+cat two.txt
+rsync -avzp /home/s4d1/b.txt cs17d11@192.168.0.30:
+~/.ssh
+rsync -zvp /home/s4d1/b.txt /home/s4d1/Desktop/a.txt
+ls -l
+
+exit
+
